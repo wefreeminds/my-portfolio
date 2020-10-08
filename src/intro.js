971f8c7bc179b70858e80b6e07c0c8cf9d0ff947
Israel Wilson <31997700+wefreeminds@users.noreply.github.com>
2020-10-08 12:25:24 -0400

    Update intro.js
    Intro Update 1.0

--- src/intro.js
+++ src/intro.js
@@ -7,8 +7,8 @@ export const Intro = (props) => {
   return (
     <section className="row justify-content-md-center">
       <div className="col-md-6 intro">
-        <h1 className="intro-text-top">Hi! I'm Nick.</h1>
-        <h1>A software engineer who builds the Future with Ethereum.</h1>
+        <h1 className="intro-text-top">Hi! I'm Israel.</h1>
+        <h1>A business development specialist and project manager who builds the Future with Ethereum.</h1>
         <IntroExtra />
       </div>
     </section>
