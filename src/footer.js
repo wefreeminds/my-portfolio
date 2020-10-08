cad7815c681a1b5a0fce51939ffe55094f1562fb
Israel Wilson <31997700+wefreeminds@users.noreply.github.com>
2020-10-08 12:15:06 -0400

    Update footer.js
    Footer Update 1.0

--- src/footer.js
+++ src/footer.js
@@ -5,7 +5,7 @@ export const Footer = (props) => {
 
   return (
     <footer ref={hireRef} className="footer">
-      <p>© Nick Zbiegien 2020</p>
+      <p>© Israel Wilson 2020</p>
       <a className="footer-code" href="https://github.com/deployAt/portfolio" target="_blank" rel="noopener noreferrer">source code</a>
     </footer>
   )
